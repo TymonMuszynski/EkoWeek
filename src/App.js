37d3534888afdcbd937dbb6dbf94880699ba4db6
Tymon Muszynski <tymonmuszynski@Tymons-MacBook-Pro.local>
2021-02-26 16:40:10 +0100

    FriFeb26

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "./components/NavBar";
 import { Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Warsztaty from "./pages/Warsztaty";
 import Video from "./pages/Video";
+import ReactGa from "react-ga";
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <NavBar></NavBar>
-        <div className="App">
-          <Switch>
-            <Route path="/video" component={Video} />
-            <Route path="/Warsztaty" component={Warsztaty} />
-            <Route path="/" component={Home} />
-          </Switch>
-        </div>
-      </>
-    );
-  }
+function App() {
+  useEffect(() => {
+    ReactGa.initialize("G-8MWE4MM8FP");
+    ReactGa.pageview(window.location.pathname + window.location);
+  }, []);
+
+  return (
+    <>
+      <NavBar></NavBar>
+      <div className="App">
+        <Switch>
+          <Route path="/video" component={Video} />
+          <Route path="/Warsztaty" component={Warsztaty} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </div>
+    </>
+  );
 }
 
 export default App;
